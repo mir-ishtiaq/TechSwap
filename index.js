59d08f18ac167d1bbad2dd76c25e0bfb799cf8ac
Mir Ishtiaq Uddin <108404327+mir-ishtiaq@users.noreply.github.com>
2023-08-25 19:49:54 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Set up MongoDB connection
+mongoose.connect('mongodb://localhost/techswap', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// Define user schema and model
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+const User = mongoose.model('User', userSchema);
+
+app.use(express.json());
+
+// User registration route
+app.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = new User({ username, password });
+    await user.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'An error occurred' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
